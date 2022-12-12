53ed51b4142c8542419f9fcb13c2561474b22a05
cooperstrite <120342565+cooperstrite@users.noreply.github.com>
2022-12-11 19:45:00 -0500

    2 files modified, 3 deleted

--- main.ts
+++ main.ts
@@ -1,23 +1,9 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let app : HTMLCanvasElement = document.querySelector('#app');
+app.style = "border: 5px solid white"
 
-setupCounter(document.querySelector('#counter'))
+const ctx = app.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(500, 500, 100, 100);
